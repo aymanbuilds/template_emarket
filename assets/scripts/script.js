bff1f09389704df148ec5e412bcf51d3a108597f
Ayman Atif <appoasis2023@gmail.com>
2024-05-26 00:16:48 +0100

    Add files via upload

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,13 +1,34 @@
-var mobileNav = document.getElementById('mobileNav');
+document.addEventListener('DOMContentLoaded', function () {
+    var mobileNav = document.getElementById('mobileNav');
 
-function showMenu() {
-    if (mobileNav) {
-        mobileNav.classList.add('show');
+    window.showMenu = function () {
+        if (mobileNav) {
+            mobileNav.classList.add('show');
+        }
     }
-}
 
-function hideMenu() {
-    if (mobileNav) {
-        mobileNav.classList.remove('show');
+    window.hideMenu = function () {
+        if (mobileNav) {
+            mobileNav.classList.remove('show');
+        }
     }
-}
+
+    var mainNav = document.getElementById('mainNav');
+    var mainMobileNav = document.getElementById('mainMobileNav');
+
+    if (!mainNav || !mainMobileNav) {
+        return;
+    }
+
+    var observer = new IntersectionObserver(function (entries) {
+        var entry = entries[0];
+
+        if (!entry.isIntersecting) {
+            mainMobileNav.classList.add('active');
+        } else {
+            mainMobileNav.classList.remove('active');
+        }
+    });
+
+    observer.observe(mainNav);
+});
