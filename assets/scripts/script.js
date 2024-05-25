46420df2b87e437d398312b048841987ea57e40f
Ayman Atif <appoasis2023@gmail.com>
2024-05-26 00:58:51 +0100

    Add files via upload

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -31,4 +31,53 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     observer.observe(mainNav);
+
+    function hideElements(containerId) {
+        const elementsToHide = document.querySelectorAll(`#${containerId} *`);
+        elementsToHide.forEach((element, index) => {
+            if (index > 0) {
+                element.classList.add('hidden');
+            }
+        });
+    }
+
+    hideElements('mainHeaderWrapper');
+    hideElements('about');
+    hideElements('features');
+    hideElements('pricing');
+    hideElements('contact');
+    hideElements('mainFooter');
+
+    function handleAnimation(entries, observer) {
+        entries.forEach(entry => {
+            const children = entry.target.querySelectorAll('*');
+            if (entry.isIntersecting) {
+                let interval = 40;
+                children.forEach(child => {
+                    setTimeout(() => {
+                        child.classList.add('animate');
+                    }, interval);
+                    interval += 10;
+                });
+            }
+            else {
+                children.forEach(child => {
+                    child.classList.remove('animate');
+                });
+            }
+        });
+    }
+
+    const visibilityObserver = new IntersectionObserver(handleAnimation, {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.2
+    });
+
+    visibilityObserver.observe(document.getElementById('mainHeaderWrapper'));
+    visibilityObserver.observe(document.getElementById('about'));
+    visibilityObserver.observe(document.getElementById('features'));
+    visibilityObserver.observe(document.getElementById('pricing'));
+    visibilityObserver.observe(document.getElementById('contact'));
+    visibilityObserver.observe(document.getElementById('mainFooter'));
 });
